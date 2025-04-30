9bc2b1aabcb0bf6a71f64bcec14ca008ecf146b8
maphnew <zcm3118@gmail.com>
2025-04-30 23:00:05 +0900

    fix: 추가된 comment add, update, delete, likes 에 setQueryData로 캐시를 직접 수정

--- src/entities/comment/api/mutations.ts
+++ src/entities/comment/api/mutations.ts
@@ -5,6 +5,8 @@ import { Comment, CommentAddDTO } from "../model/types"
 import { LikeCommentProps } from "../model/types"
 import { Post, PostComments } from "../../post/model"
 
+const erroMessage = "API 데이터에 해당 댓글이 존재하지 않습니다. (하지만 앱 작동을 위해 캐시 데이터는 수정됩니다)"
+
 export const useAddComment = (postId: Post["id"]) => {
   const queryClient = useQueryClient()
   return useMutation({
@@ -23,7 +25,7 @@ export const useAddComment = (postId: Post["id"]) => {
         }
       })
     },
-    onError: (error) => {
+    onError: (error: Error, variables: CommentAddDTO, context: unknown) => {
       console.error("댓글 추가 오류:", error)
     },
   })
@@ -49,8 +51,17 @@ export const useUpdateComment = (postId: Post["id"]) => {
         }
       })
     },
-    onError: (error) => {
-      console.error("댓글 업데이트 오류:", error)
+    onError: (error: Error, variables: Comment, context: unknown) => {
+      queryClient.setQueriesData({ queryKey: commentKeys.list(postId) }, (prev: PostComments) => {
+        return {
+          ...prev,
+          comments: prev.comments.map((comment) => {
+            if (comment.id === variables.id) return variables
+            return comment
+          }),
+        }
+      })
+      console.error("댓글 업데이트 오류:" + erroMessage, error)
     },
   })
 }
@@ -75,8 +86,17 @@ export const useLikeComment = (postId: Post["id"]) => {
         }
       })
     },
-    onError: (error) => {
-      console.error("댓글 좋아요 오류:", error)
+    onError: (error: Error, variables: LikeCommentProps, context: unknown) => {
+      queryClient.setQueriesData({ queryKey: commentKeys.list(postId) }, (prev: PostComments) => {
+        return {
+          ...prev,
+          comments: prev.comments.map((comment) => {
+            if (comment.id === variables.commentId) return { ...comment, likes: comment.likes ? comment.likes + 1 : 1 }
+            return comment
+          }),
+        }
+      })
+      console.error("댓글 좋아요 오류:" + erroMessage, error)
     },
   })
 }
@@ -100,8 +120,17 @@ export const useDeleteComment = (postId: Post["id"]) => {
         }
       })
     },
-    onError: (error) => {
-      console.error("댓글 삭제 오류:", error)
+    onError: (error: Error, variables: number | void, context: unknown) => {
+      queryClient.setQueriesData({ queryKey: commentKeys.list(postId) }, (prev: PostComments) => {
+        return {
+          ...prev,
+          comments: prev.comments.filter((comment) => {
+            if (comment.id === variables) return false
+            return true
+          }),
+        }
+      })
+      console.error("댓글 삭제 오류:" + erroMessage, error)
     },
   })
 }
